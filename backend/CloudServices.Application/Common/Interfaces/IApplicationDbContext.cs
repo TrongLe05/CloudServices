8b714165ce70ec249a9b53572eeee3f5af3b8eb8
SettingNo01 <127822444+TrongLe05@users.noreply.github.com>
2026-08-18 23:05:40 +0700

    Update IApplicationDbContext.cs

--- backend/CloudServices.Application/Common/Interfaces/IApplicationDbContext.cs
+++ backend/CloudServices.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -7,13 +7,16 @@ public interface IApplicationDbContext
 {
     DbSet<Role> Roles { get; }
     DbSet<AppUser> AppUsers { get; }
+    DbSet<ServiceCategory> ServiceCategories { get; }
     DbSet<ServicePlan> ServicePlans { get; }
     DbSet<Promotion> Promotions { get; }
     DbSet<PlanPrice> PlanPrices { get; }
     DbSet<OrderRequest> OrderRequests { get; }
     DbSet<AffiliateApplication> AffiliateApplications { get; }
     DbSet<AuditLog> AuditLogs { get; }
-    DbSet<NewsArticle> NewsArticles { get; } // Added NewsArticles
+    DbSet<NewsArticle> NewsArticles { get; }
+    DbSet<Testimonial> Testimonials { get; }
 
     Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    int SaveChanges();
 }
